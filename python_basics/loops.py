4daf0a42a19651272215651fe3c8b27410d4c201
Germain Tagne <bigclerie7@gmail.com>
2026-01-30 15:37:46 +0100

    Add loop examples

---
+++ python_basics/loops.py
@@ -0,0 +1,11 @@
+# Loops in Python
+
+# For loop
+for i in range(1, 6):
+    print("For loop iteration:", i)
+
+# While loop
+count = 1
+while count <= 5:
+    print("While loop count:", count)
+    count += 1
