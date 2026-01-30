3d0890a7929e5cfaf978741fb0a072042af25de2
Germain Tagne <bigclerie7@gmail.com>
2026-01-30 15:30:45 +0100

    Add conditional statements example

---
+++ python_basics/conditions.py
@@ -0,0 +1,10 @@
+# Conditions in Python
+
+temperature = 30
+
+if temperature > 25:
+    print("It is hot today")
+elif temperature > 15:
+    print("The weather is nice")
+else:
+    print("It is cold today")
