8cdd1b4a5fa0e96c611d67b92a08be5f73f4fec7
Germain Tagne <bigclerie7@gmail.com>
2026-01-30 15:35:55 +0100

    Add variables and data types examples

---
+++ python_basics/variables_types.py
@@ -0,0 +1,11 @@
+# Variables and Data Types
+
+name = "Germain"
+age = 22
+height = 1.75
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
